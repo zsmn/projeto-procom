cf0da88c87b6df8252605eb57ef6d4b6353096cc
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2021-03-29 21:05:26 -0300

    Update protocol.py

--- protocol/protocol.py
+++ protocol/protocol.py
@@ -14,20 +14,43 @@ class Protocol:
         self.serverSocket = serverSocket
         self.clientSocket = clientSocket
 
-    def handShake(self):
+    def handshake(self):
+        # Client send auth message to Server [auth(clientPublicKey, encryptPublicServer(nonce))]
+        nonce = RSA.generate(1024, Random.new().read)
+        auth = {'clientPublicKey': self.clientSocket.publicKey, 'encryptedNonce': self.clientSocket.encryptMessage(nonce, self.serverSocket.publicKey)}
+				authAsString = str(auth)
+        self.clientSocket.sendMessage(self.serverSocket, authAsString)
         
-
+        # Server receive auth message from Client and decrypt the nonce
+        data = eval(self.serverSocket.readMessage(self.clientSocket))
+        decryptedNonce = self.serverSocket.decryptMessage(data['encryptedNonce'], self.serverSocket.privateKey)
+        
+        # Server send decrypted nonce to Client, but now encrypted with Client public key
+        self.serverSocket.sendMessage(self.clientSocket, self.serverSocket.encryptMessage(decryptedNonce, data['clientPublicKey']))
+        
+        # Client receive decrypted nonce [now encrypted by its public key] from Server and process it 
+				encryptedNonce = self.clientSocket.readMessage(self.serverSocket)
+        decryptedNonce = self.clientSocket.decryptMessage(encryptedNonce, self.clientSocket.privateKey)
+				everythingOk = self.clientSocket.checkNonce(nonce, decryptedNonce)
+        if everythingOk == True:
+        		self.clientSocket.sendMessage(self.serverSocket, "ack")
+        else:
+        		self.clientSocket.sendMessage(self.serverSocket, "nack")
+				return everythingOk
+      
     def generateSecretKey(self):
-        self.secretKey = os.urandom(16)
-
+        self.secretKey = os.urandom(16) # symmetric key
+ 
     def voteSession(self):
-
+    		pass
 
 class Socket:
     def __init__(self, address, port):
         self.address = address
         self.port = port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.publicKey = ""
+        self.privateKey = ""
         self.generateKeys()
 
     def connectServer(self):
@@ -51,3 +74,13 @@ class Socket:
     def generateKeys(self):
         self.privateKey = RSA.generate(1024, Random.new().read)
         self.publicKey = self.privateKey.publickey()
+    
+    def readMessage(self, senderSocket):
+    		info = senderSocket.recv(1024)
+   			return info
+      
+    def checkNonce(self, generatedNonce, receivedNonce):
+    		if generatedNonce == receivedNonce:
+        		return True
+    		else:
+        		return False
